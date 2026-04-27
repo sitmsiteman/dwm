4fe4136649979597459577647f6cc69aef61755b
sitmsiteman <sitmsiteman@gmail.com>
2026-04-27 19:25:24 +0900

    update printscreen
    require maim and xdg-user-dir

--- config.def.h
+++ config.def.h
@@ -75,6 +75,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *screenshotcmd[]  = { "xshot.sh", NULL };
 static const char *dpmsoffcmd[] = { "xset", "dpms", "force", "off", NULL };
 static const char *upbrightness[]   = { "brightnessctl", "set", "+5%", NULL };
 static const char *downbrightness[] = { "brightnessctl", "set", "5%-", NULL };
@@ -126,6 +127,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = +0.25} },
 	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = -0.25} },
 	{ MODKEY|ShiftMask,             XK_o,      setcfact,       {.f =  0.00} },
+	{ 0,            	XK_Print, 		spawn,          {.v = screenshotcmd } },
 };
 
 /* button definitions */
