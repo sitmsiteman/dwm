acb4270b46a24c81c6b5b794852c40393ef21491
sitmsiteman <sitmsiteman@gmail.com>
2026-04-28 22:09:29 +0900

    toggle dpms to Mod + q

--- config.def.h
+++ config.def.h
@@ -77,6 +77,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *screenshotcmd[]  = { "xshot.sh", NULL };
+static const char *toggledpmscmd[]  = { "toggle-dpms", NULL };
 static const char *dpmsoffcmd[] = { "xset", "dpms", "force", "off", NULL };
 static const char *upbrightness[]   = { "brightnessctl", "set", "+5%", NULL };
 static const char *downbrightness[] = { "brightnessctl", "set", "5%-", NULL };
@@ -129,6 +130,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = -0.25} },
 	{ MODKEY|ShiftMask,             XK_o,      setcfact,       {.f =  0.00} },
 	{ 0,            	XK_Print, 		spawn,          {.v = screenshotcmd } },
+	{ MODKEY,            	XK_q, 		spawn,          {.v = toggledpmscmd } },
 };
 
 /* button definitions */
