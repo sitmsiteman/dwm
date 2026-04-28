bee1459b6651aeb63fb946423501f6fb73f9dd0e
sitmsiteman <sitmsiteman@gmail.com>
2026-04-29 05:03:19 +0900

    update config

--- config.def.h
+++ config.def.h
@@ -32,7 +32,7 @@ static const char *const autostart[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "Term1", "Term2", "FM", "S", "G", "Docs", "Media", "Emacs", "Web" , "Scratch"};
+static const char *tags[] = { "1", "2", "3", "FM", "Full", "Docs", "Media", "Emacs", "Web" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -45,12 +45,12 @@ static const Rule rules[] = {
 	{ "Emacs",  NULL,       NULL,       1 << 7,	    0,           -1 },
 	{ "mpv",  NULL,       NULL,       1 << 6,	    0,           -1 },
 	{ "Zathura",  NULL,       NULL,       1 << 5,	    0,           -1 },
-	{ "steam",  "steamwebhelper",       NULL,       1 << 3,	    0,           -1 },
+	{ "steam",  "steamwebhelper",       NULL,       0,	    0,           -1 },
 	{ "steam_app",  "steam_app",       NULL,       1 << 4,	    0,           -1 },
 	{ "st-256color",  NULL,       NULL,       0,	    0,           -1 },
 	{ "TelegramDesktop",  NULL,   NULL,  0,            1,           -1 },
 	{ "xdg-desktop-portal-lxqt",     NULL,       NULL,   0,    1,      -1 },
-	{ "Pcmanfm",     NULL,       NULL,       1 << 2,            0,           -1 },
+	{ "Pcmanfm",     NULL,       NULL,       1 << 3,            0,           -1 },
 };
 
 /* layout(s) */
