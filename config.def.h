6d3c1ffc00889b0a299a9a76b4e6a2f64f2a7735
sitmsiteman <sitmsiteman@gmail.com>
2026-05-04 17:34:42 +0900

    pseudo-plan9 style

--- config.def.h
+++ config.def.h
@@ -3,7 +3,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows (original: 1) */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
@@ -14,17 +14,25 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Noto Sans CJK KR:size=10" };
 static const char dmenufont[]       = "Noto Sans CJK KR:size=10";
+/* Rio colorscheme */
+static const char col_rio_bg[]      = "#e9ffe9";
+static const char col_rio_fg[]      = "#000000";
+static const char col_rio_act[]     = "#55aaaa";
+static const char col_rio_inact[]   = "#9eeeee";
+
+/* Original colors kept for reference
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
+*/
 
+static const char *colors[][3]      = {
+	/*               fg          bg             border */
+	[SchemeNorm] = { col_rio_fg, col_rio_bg,    col_rio_inact }, /* orig: col_gray3, col_gray1, col_gray2 */
+	[SchemeSel]  = { col_rio_fg, col_rio_inact, col_rio_act   }, /* orig: col_gray4, col_cyan,  col_cyan  */
+};
 static const char *const autostart[] = {
 	"st", "-c", "st-main", "-e", "tmux", "new-session", "-A", "-s", "main", NULL,
 	"st", "-c", "st-remote", "-e", "tmux", "new-session", "-A", "-s", "remote", NULL,
@@ -101,7 +109,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+/* static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL }; */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_rio_bg, "-nf", col_rio_fg, "-sb", col_rio_act, "-sf", col_rio_fg, NULL };
 static const char *termcmd[]  = { "st", "-e", "tmux", "new-session", "-A", "-s", "sub", NULL };
 static const char *screenshotcmd[]  = { "xshot.sh", NULL };
 static const char *toggledpmscmd[]  = { "toggle-dpms", NULL };
